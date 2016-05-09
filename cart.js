8eaba61f70387c5b43ca3b84694faccef8784c74
Ilya Bukanov <ibukanov87@gmail.com>
2016-05-09 20:48:51 +0300

    impl Lab 5.

--- cart.js
+++ cart.js
@@ -37,20 +37,17 @@ function CartDAO(database) {
         * callback function.
         *
         */
-
-        var userCart = {
-            userId: userId,
-            items: []
-        }
-        var dummyItem = this.createDummyItem();
-        userCart.items.push(dummyItem);
-
+        this.db.collection("cart")
+            .find({userId: userId})
+            .limit(1)
+            .next(function(err, cart) {
+                callback(cart);
+            });
         // TODO-lab5 Replace all code above (in this method).
 
         // TODO Include the following line in the appropriate
         // place within your code to pass the userCart to the
         // callback.
-        callback(userCart);
     }
 
 
